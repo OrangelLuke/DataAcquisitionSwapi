075005c7c1b85e1abba1ff3c1231baf08e0a5d86
OrangelLuke <angelator2001@gmail.com>
2024-01-04 18:58:34 +0100

    Code to get all characters and print their names

--- extractData.py
+++ extractData.py
@@ -1,23 +1,19 @@
 import requests
 
-URL_BASE = "https://swapi.co/api/"
+URL_BASE = "https://swapi.dev/api/"
 r = requests.get(URL_BASE + 'people/')
 if r.status_code == 200:
-	doc = r.text
-	print(doc)
-	print("Número de personajes:", doc["count"])
-	for resultado in doc["results"]:
-		print(resultado["name"])
-		pagina = 2
-		tecla = input("Pulsa espacio para continuar o otra tecla para salir...")
-		while doc["next"] is not None and tecla == " ":
-			payload = {"page": pagina}
-			r = requests.get(URL_BASE + 'people/', params=payload)
-			if r.status_code == 200:
-				doc = r.json()
-				for resultado in doc["results"]:
-					print(resultado["name"])
-					pagina = pagina + 1
-					tecla = input("Pulsa espacio para continuar o otra tecla para salir...")
+	doc = r.json()
+	print("Number of characters:", doc["count"])
+	for result in doc["results"]:
+		print(result["name"])
+	page = 2
+	while doc["next"] is not None:
+		r = requests.get(doc["next"])
+		if r.status_code == 200:
+			doc = r.json()
+			for result in doc["results"]:
+				print(result["name"])
+			page = page + 1
 else:
-	print("Error en la API")
+	print("API error")
